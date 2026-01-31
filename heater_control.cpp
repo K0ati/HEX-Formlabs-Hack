31dda83051c6fa7a059a7089a96376ebe02f1597
Naima Aubry-Romero <naima.aubry.usa@gmail.com>
2026-01-31 02:30:25 -0500

    heater code

---
+++ heater_control.cpp
@@ -0,0 +1,27 @@
+// pins
+const int HEATER_PIN = 7;   // MOSFET gate
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("System starting...");
+  Serial.println("Send '0' to turn heater OFF");
+  Serial.println("Send '1' to turn heater ON");
+  pinMode(HEATER_PIN, OUTPUT);
+  digitalWrite(HEATER_PIN, LOW);  // start off
+}
+
+void loop() {
+  if (Serial.available()) {
+    char cmd = Serial.read();
+
+    if (cmd == '1') {
+      Serial.println("Heater ON");
+      digitalWrite(HEATER_PIN, HIGH);
+    }
+    else if (cmd == '0') {
+      Serial.println("Heater OFF");
+      digitalWrite(HEATER_PIN, LOW);
+    }
+  }
+}
